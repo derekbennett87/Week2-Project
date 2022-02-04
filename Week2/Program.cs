37ef619518965dae9c670cdafb41788bf8371d6a
derekbennett87 <98566408+derekbennett87@users.noreply.github.com>
2022-02-04 12:30:27 -0600

    Week 2 Project

---
+++ Week2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Week_2_Project
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int myVar = 7;
+
+            Console.WriteLine(-1+4*myVar);
+
+            Console.WriteLine((35+5)%myVar);
+
+            Console.WriteLine(14+-4*6/12);
+
+            Console.WriteLine(2 + 12 / 6 * 1 - myVar % 2);
+
+            if (myVar * myVar < 10) 
+            {
+                Console.WriteLine("myVar is less than 4"); 
+            }
+
+         else
+                Console.WriteLine("myVar is greater than 4");
+        }
+        
+    }
+   
+} 
